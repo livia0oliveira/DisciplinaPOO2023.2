bbf3f3dc5b36176f513a38185bd4e87d5f3062bf
Ana Livia Oliveira <143213393+livia0oliveira@users.noreply.github.com>
2023-08-29 01:35:22 -0300

    Add files via upload

---
+++ Lista01/q23r/src/edu/br/principal/Principal.java
@@ -0,0 +1,20 @@
+package edu.br.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    // QUESTAO 23
+	    
+	    // declarar variáveis
+	    float num;
+	    int i, f, a;
+	    
+	    // leitura
+	    num = sc.nextInt();
+	    
+	    
+	    
+	}
+}
